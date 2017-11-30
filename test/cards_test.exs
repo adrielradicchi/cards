80ab0ea82159dea86a5447c45df154cfa1c0ef3a
Adriel Radicchi <adriel.nfsc@gmail.com>
2017-11-30 12:42:28 -0200

    Update module from test in project

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -6,5 +6,16 @@ defmodule CardsTest do
   #  assert Cards.hello() == :world
   #end
 
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+  
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    #assert deck != Cards.shuffle(deck) 
+    refute deck == Cards.shuffle(deck) 
+  end
+
   
 end
